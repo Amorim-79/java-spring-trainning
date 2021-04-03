4b369eb0038038abb280b204ea9c3c5a107963b9
Pedro Henrique <pedro.henrique@helpper.com.br>
2021-04-03 19:54:24 -0300

    feature: created query sql to delete a product

--- jdbc-course/src/br/com/amorim/jdbc/DeleteData.java
+++ jdbc-course/src/br/com/amorim/jdbc/DeleteData.java
@@ -1,5 +1,18 @@
 package br.com.amorim.jdbc;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DeleteData {
 
+public static void main(String[] args) throws SQLException {
+		
+		Connection connection = new ConnectionFactory().getConnection();
+		
+		Statement statement = connection.createStatement();
+		statement.execute("DELETE FROM product WHERE id LIKE '2'");
+		connection.close();
+		
+	}
 }
