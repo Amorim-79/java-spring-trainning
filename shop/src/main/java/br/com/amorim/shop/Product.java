f6316c7b35087d6c448eb70b0e1a70990cbd1a18
Pedro Henrique <pedro.henrique@helpper.com.br>
2021-04-03 17:58:15 -0300

    feature: created test of Product class and added new plugin's to maven project

--- shop/src/main/java/br/com/amorim/shop/Product.java
+++ shop/src/main/java/br/com/amorim/shop/Product.java
@@ -1,5 +1,27 @@
 package br.com.amorim.shop;
 
 public class Product {
+	private int id;
+	private String userName;
+	
+	public Product(int id, String userName) {
+		this.id = id;
+		this.userName = userName;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 }
--- shop/src/test/java/br/com/amorim/shop/ProductTest.java
+++ shop/src/test/java/br/com/amorim/shop/ProductTest.java
@@ -1,14 +1,24 @@
 package br.com.amorim.shop;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ProductTest {
-
+		
+	Product product = new Product(2, "Tester");
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void creatingNewProduct() {	
+		Assert.assertEquals(2, product.getId());
+		Assert.assertEquals("Tester", product.getUserName());
 	}
-
+	
+	@Test
+	public void changeProductData() {
+		product.setId(3);
+		product.setUserName("changedTester");
+		
+		Assert.assertEquals(3, product.getId());
+		Assert.assertEquals("changedTester", product.getUserName());
+	}
 }
